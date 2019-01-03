14c31fd155d9e332ede5a9df0fcb5e0ffb713ad0
Lars Thise Pedersen <ltpe@techcollege.dk>
2019-01-03 12:48:30 +0100

    Nu mulighed for at oprette nye sange.

--- SongTest/Controllers/SongController.cs
+++ SongTest/Controllers/SongController.cs
@@ -20,7 +20,33 @@ namespace SongTest.Controllers
 
         public ActionResult CreateSong()
         {
+            List<Genre> GenreList = db_Object.Genres.ToList();
+            return (View(GenreList));
+        }
 
+        [HttpPost]
+        public ActionResult CreateSong(Song Song_Object)
+        {
+            int NumberOfRecordsSaved = 0;
+
+            if (ModelState.IsValid)
+            {
+                db_Object.Songs.Add(Song_Object);
+                NumberOfRecordsSaved = db_Object.SaveChanges();
+
+                if (NumberOfRecordsSaved > 0)
+                {
+                    return (RedirectToAction("SongList", "Song"));
+                }
+                else
+                {
+                    return (RedirectToAction("Index", "Home"));
+                }
+            }
+            else
+            {
+                return (View());
+            }
         }
     }
 }
